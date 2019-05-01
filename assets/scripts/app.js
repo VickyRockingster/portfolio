71649d980d00dca263d359a105b3917e989ddd42
vickyrockingster <vrockingster@gmail.com>
2019-04-30 23:47:33 -0400

    Cleaned up the out of date comments

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,7 +11,6 @@ $(() => {
     const winTop = $(window).scrollTop()
     if (winTop >= 30) {
       $('body').addClass('sticky-shrinknav-wrapper')
-      // $('main').
     } else {
       $('body').removeClass('sticky-shrinknav-wrapper')
     }
