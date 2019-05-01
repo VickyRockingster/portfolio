558e97124aebf6a8a03f299c48bbd96766c9a97d
vickyrockingster <vrockingster@gmail.com>
2019-04-30 23:42:32 -0400

    Commented out the loaders I added for the icons

--- grunt/webpack.js
+++ grunt/webpack.js
@@ -89,13 +89,13 @@ module.exports = {
             ]
           }
         }
-      ],
-      loaders: [
-        // the url-loader uses DataUrls.
-        // the file-loader emits files.
-        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
       ]
+      // loaders: [
+      //   // the url-loader uses DataUrls.
+      //   // the file-loader emits files.
+      //   { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      //   { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
+      // ]
     },
 
     resolve: {
